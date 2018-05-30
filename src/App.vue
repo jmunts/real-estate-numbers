f49905b603058ca419f2fb4677beeb85808b2fc5
John Mark Untalan <apeiron.jmunts@gmail.com>
2018-05-30 19:55:55 +0800

    Add amortization calculator

--- src/App.vue
+++ src/App.vue
@@ -30,17 +30,17 @@
                         />
                       </v-flex>
                       <v-flex xs12 md4>
-                        <v-text-field
+                        <v-select
                           label="Interest rate"
-                          suffix="%"
                           v-model="interestRate"
+                          :items="interestRateOptions"
                         />
                       </v-flex>
                       <v-flex xs12 md4>
-                        <v-text-field
+                        <v-select
                           label="Payable in?"
                           v-model="payableInYears"
-                          suffix="year(s)"
+                          :items="payableInYearsOptions"
                         />
                       </v-flex>
                       <v-flex xs12 v-if="showMonthlyAmortization">
@@ -59,18 +59,18 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import { amortizationFactorRateTable } from './helpers/amortization-factor-rate-table.js'
+
 export default {
   name: 'App',
   data () {
     return {
       price: 1703000,
       downpaymentPct: 10,
-      interestRate: 13,
       payableInYears: 15,
-      amortizationFactorRateTable: [
-        // TODO
-      ],
-      amortizationFactorRate: 0.012652422
+      interestRate: '13.0',
+      amortizationFactorRateTable: amortizationFactorRateTable
     }
   },
   computed: {
@@ -78,10 +78,27 @@ export default {
       return this.price !== '' && this.downpaymentPct !== '' && this.payableInYears !== '' && this.interestRate !== ''
     },
     monthlyAmortization () {
-      // TODO: when amortizationFactorRate is ready do: netPrice * amortizationFactorRate
       var downpaymentPrice = this.price * (this.downpaymentPct / 100)
       var netPrice = this.price - downpaymentPrice
       return netPrice * this.amortizationFactorRate
+    },
+    amortizationFactorRate () {
+      var rate = this.amortizationFactorRateTable[this.payableInYears - 1][this.interestRate]
+      console.log(rate)
+      return _.toNumber(rate)
+    },
+    payableInYearsOptions () {
+      return _.range(1, 31).map(n => {
+        var year = n === 1 ? 'year' : 'years'
+        return { text: `${n} ${year}`, value: n }
+      })
+    },
+    interestRateOptions () {
+      let rates = ['1.0', '1.25', '1.5', '1.75', '2.0', '2.25', '2.5', '2.75', '3.0', '3.25', '3.5', '3.75', '4.0', '4.25', '4.5', '4.75', '5.0', '5.25', '5.5', '5.75', '6.0', '6.25', '6.5', '6.75', '7.0', '7.25', '7.5', '7.75', '8.0', '8.25', '8.5', '8.75', '9.0', '9.25', '9.5', '9.75', '10.0', '10.25', '10.5', '10.75', '11.0', '11.25', '11.5', '11.75', '12.0', '12.25', '12.5', '12.75', '13.0', '13.25', '13.5', '13.75', '14.0', '14.25', '14.5', '14.75', '15.0', '15.25', '15.5', '15.75', '16.0', '16.25', '16.5', '16.75', '17.0', '17.25', '17.5', '17.75', '18.0', '18.25', '18.5', '18.75', '19.0', '19.25', '19.5', '19.75', '20.0', '20.25', '20.5', '20.75']
+
+      return rates.map(rate => {
+        return { text: `${rate}%`, value: rate }
+      })
     }
   },
   filters: {
